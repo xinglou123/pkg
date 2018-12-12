2046239170a05e71ce3c9350d46feb90d4cdd497
cbliu <chengbin_liu@hotmail.com>
2018-12-12 11:55:41 +0800

    //todo:lcb add db

--- os/md5/xmd5.go
+++ os/md5/xmd5.go
@@ -2,9 +2,11 @@ package md5
 
 import (
 	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"golang.org/x/crypto/scrypt"
 	"io"
 	"os"
 	"reflect"
@@ -52,4 +54,51 @@ func EncodeFile(path string) string {
 		fmt.Errorf("%s", e)
 	}
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// GenRandomString 生成随机字符串
+// length 生成长度
+// specialChar 是否生成特殊字符
+func GenRandomString(length int, specialChar bool) string {
+
+	letterBytes := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special := "!@#%$*.="
+
+	if specialChar {
+		letterBytes = letterBytes + special
+	}
+
+	chars := []byte(letterBytes)
+
+	if length == 0 {
+		return ""
+	}
+
+	clen := len(chars)
+	maxrb := 255 - (256 % clen)
+	b := make([]byte, length)
+	r := make([]byte, length+(length/4)) // storage for random bytes.
+	i := 0
+	for {
+		if _, err := rand.Read(r); err != nil {
+			return ""
+		}
+		for _, rb := range r {
+			c := int(rb)
+			if c > maxrb {
+				continue // Skip this number to avoid modulo bias.
+			}
+			b[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(b)
+			}
+		}
+	}
+}
+
+// CryptPassword 加密密码
+func CryptPassword(password, salt string) string {
+	dk, _ := scrypt.Key([]byte(password), []byte(salt), 16384, 8, 1, 32)
+	return fmt.Sprintf("%x", dk)
 }
