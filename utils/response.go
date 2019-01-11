e3a861bef4247e71cf7ceab29522a573d5a2ff4d
cbliu <chengbin_liu@hotmail.com>
2019-01-11 14:09:55 +0800

    //todo:lcb add validator

--- utils/response.go
+++ utils/response.go
@@ -2,16 +2,18 @@ package utils
 
 // Response represents HTTP response body.
 type Response struct {
-	Code int         `json:"code"` // return code, 0 for succ
-	Msg  string      `json:"msg"`  // message
-	Data interface{} `json:"data"` // data object
+	Code  int         `json:"code"`  // return code, 0 for succ
+	Msg   string      `json:"msg"`   // message
+	Data  interface{} `json:"data"`  // data object
+	Total int         `json:"total"` // return code, 0 for succ
 }
 
 // NewResponse creates a result with Code=0, Msg="", Data=nil.
 func NewResponse() *Response {
 	return &Response{
-		Code: 200,
-		Msg:  "",
-		Data: nil,
+		Code:  200,
+		Msg:   "",
+		Data:  nil,
+		Total: 0,
 	}
 }
